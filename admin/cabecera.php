5d999b2052d1b72c5feb75213a53dc013694d754
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-02-07 23:58:38 -0500

    rservas
    dsdsd

---
+++ admin/cabecera.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <meta charset="utf-8">
+    <title>Reservas</title>
+    
+
+    <!-- The styles -->
+    <link id="bs-css" href="../css/bootstrap-cerulean.min.css" rel="stylesheet">
+
+    <link href="../css/charisma-app.css" rel="stylesheet">
+    <link href='../bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
+    <link href='../bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
+    <link href='../bower_components/chosen/chosen.min.css' rel='stylesheet'>
+    <link href='../bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
+    <link href='../bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='../bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='../css/jquery.noty.css' rel='stylesheet'>
+    <link href='../css/noty_theme_default.css' rel='stylesheet'>
+    <link href='../css/elfinder.min.css' rel='stylesheet'>
+    <link href='../css/elfinder.theme.css' rel='stylesheet'>
+    <link href='../css/jquery.iphone.toggle.css' rel='stylesheet'>
+    <link href='../css/uploadify.css' rel='stylesheet'>
+    <link href='../css/animate.min.css' rel='stylesheet'>
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+
+    <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- The fav icon -->
+    <link rel="shortcut icon" href="../img/favicon.ico">
+	
+	
+
+</head>
+
+<body>
+    <!-- topbar starts -->
+    <div class="navbar navbar-default" role="navigation">
+
+        <div class="navbar-inner">
+            <button type="button" class="navbar-toggle pull-left animated flip">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"> <img alt="Charisma Logo"  class="hidden-xs"/>
+                <span>Reservas</span></a>
+
+            <!-- user dropdown starts -->
+            <div class="btn-group pull-right">
+                <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs"> admin</span>
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Profile</a></li>
+                    <li class="divider"></li>
+                    <li><a href="login.html">Logout</a></li>
+                </ul>
+            </div>
+            <!-- user dropdown ends -->
+
+            <ul class="collapse navbar-collapse nav navbar-nav top-menu">
+                <li>
+                    <form class="navbar-search pull-left">
+                        <input placeholder="Search" class="search-query form-control col-md-10" name="query"
+                               type="text">
+                    </form>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+    <!-- topbar ends -->
+<div class="ch-container">
+    <div class="row">
+        
+        <!-- left menu starts -->
+        <div class="col-sm-2 col-lg-2">
+            <div class="sidebar-nav">
+                <div class="nav-canvas">
+                    <div class="nav-sm nav nav-stacked">
+
+                    </div>
+                    <ul class="nav nav-pills nav-stacked main-menu">
+                        <li class="nav-header">Menu</li>
+                        <li><a class="ajax-link" href="index.html"><i class="glyphicon glyphicon-home"></i><span> Reservas</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="ui.html"><i class="glyphicon glyphicon-eye-open"></i><span> Clientes</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="chart.html"><i class="glyphicon glyphicon-list-alt"></i><span> Vuelo</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="typography.html"><i class="glyphicon glyphicon-font"></i><span> Avion</span></a>
+                        </li>
+                        <li><a class="ajax-link" href="gallery.html"><i class="glyphicon glyphicon-picture"></i><span> Ruta</span></a>
+                        </li>
+                        </li>
+                        <li><a class="ajax-link" href="gallery.html"><i class="glyphicon glyphicon-picture"></i><span> Escala</span></a>
+                        </li>
+                        </li>
+                        <li><a class="ajax-link" href="gallery.html"><i class="glyphicon glyphicon-picture"></i><span> Aeropuerto</span></a>
+                        </li>
+                        </li>
+                        <li><a class="ajax-link" href="gallery.html"><i class="glyphicon glyphicon-picture"></i><span> Clase</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--/span-->
+        <!-- left menu ends -->
+
+      
+
+</body>
+</html>
