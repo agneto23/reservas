fcec51b1155b1202ada505b3dbd2440494aa1a93
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-02-14 19:33:32 -0500

    pie
    dfdffd

--- usuario/piepagina.php
+++ usuario/piepagina.php
@@ -2,8 +2,8 @@
 	<div class="main">
 <!-- footer -->
 		<footer>
-			<a rel="nofollow" href="http://www.templatemonster.com/" target="_blank">Website template</a> designed by TemplateMonster.com<br>
-			<a href="http://www.templates.com/product/3d-models/" target="_blank">3D Models</a> provided by Templates.com
+			 <h3> © 2016 VIAJA AHORA S.A.</h3>
+			 Todos los derechos reservados.
 		</footer>
 <!-- / footer -->
 	</div>
