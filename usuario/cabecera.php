4d74e81c3ecd1d3acb560bd83a8be5a842dd0129
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-02-14 22:31:46 -0500

    cabecera sin terminar
    sdsd

--- usuario/cabecera.php
+++ usuario/cabecera.php
@@ -3,6 +3,28 @@
 <head>
 <title></title>
 <meta charset="utf-8">
+
+
+   <!-- jQuery -->
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>   
+    <script type="text/javascript" src="../js/script.js"></script> 
+    <script type="text/javascript" src="../js/jquery.js"></script>
+    <script src="../js/moment.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+        
+    <script type="text/javascript" src="../js/jquery.countdown.js"></script>
+   
+    <script type="text/javascript" src="../js/ajax.js"></script>
+
+        
+    <script type="text/javascript" src="../js/bootstrap-datetimepicker.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap-datetimepicker.css">
+
+ 
+
+
 <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
 <link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
@@ -12,6 +34,10 @@
 <script type="text/javascript" src="js/Myriad_Pro_italic_600.font.js"></script>
 <script type="text/javascript" src="js/Myriad_Pro_italic_400.font.js"></script>
 <script type="text/javascript" src="js/Myriad_Pro_400.font.js"></script>
+
+
+
+
 <!--[if lt IE 9]>
 	<script type="text/javascript" src="http://info.template-help.com/files/ie6_warning/ie6_script_other.js"></script>
 	<script type="text/javascript" src="js/html5.js"></script>
@@ -29,10 +55,11 @@
 				<div class="right">
 					<nav>
 						<ul id="top_nav">
-							<li><a href="index.html"><img src="images/img1.gif" alt=""></a></li>
-							<li><a href="index-4.html"><img src="images/img2.gif" alt=""></a></li>
-							<li class="bg_none"><a href="#"><img src="images/img3.gif" alt=""></a></li>
-						</ul>
+						    <input  type="text" class="form-control" name="txtusuario"  placeholder="Ingresa tu cédula">
+						    <input  type="password" class="form-control" name="txtpassword" placeholder="Ingresa tu contraseña">
+						     <input name="login" class="btn btn-primary pull-right" type="submit" value="login">
+							
+					    </ul>
 					</nav>
 					<nav>
 						<ul id="menu">
