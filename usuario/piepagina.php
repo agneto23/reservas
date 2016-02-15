efe5a7cb2e699c3c997a3cdfc78967fb4843e8a5
desaguilar23 <carlos.andres.1994@hotmail.com>
2016-02-14 19:22:04 -0500

    principal
    sdsdss

---
+++ usuario/piepagina.php
@@ -0,0 +1,13 @@
+<div class="body2">
+	<div class="main">
+<!-- footer -->
+		<footer>
+			<a rel="nofollow" href="http://www.templatemonster.com/" target="_blank">Website template</a> designed by TemplateMonster.com<br>
+			<a href="http://www.templates.com/product/3d-models/" target="_blank">3D Models</a> provided by Templates.com
+		</footer>
+<!-- / footer -->
+	</div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
