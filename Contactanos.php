cd5709156b22c2cdbefe5c16278763a6eef7cc2e
alexMS007 <alexd_8425@hotmail.com>
2016-02-19 03:56:41 -0500

    noticia
    notocia-contactos

--- Contactanos.php
+++ Contactanos.php
@@ -1,150 +1,29 @@
 <?php include 'usuario/cabecera.php' ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<div class="images/avion.png">
-    <ul><li><a href="#" title="Vuelo Inaugural STI..."><img src="images/avion.png" alt="foto1" height="150 " /></a></li></ul>
-  </div>
- <p>&nbsp;</p>
-</td>
-<td class="right" align="left" valign="top">
-  <h2 class="custom">Formulario de contacto</h2>
-
-  <div id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_pnlContactForm">
-	
-    <table border="0" cellpadding="0" cellspacing="0" class="form">
-        <tr>
-            <td align="left" valign="top">
-                <strong>Nombre: </strong>
-            </td>
-            <td align="left" valign="top">
-                <input name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$txtNombre" type="text" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtNombre" style="width:200px;" />
-            </td>
-            <td align="left" valign="top">
-                <span id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1" style="color:Red;visibility:hidden;">*</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="left" valign="top">
-                <strong>Apellido: </strong>
-            </td>
-            <td align="left" valign="top">
-                <input name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$txtApellido" type="text" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtApellido" style="width:200px;" />
-            </td>
-            <td align="left" valign="top">
-                <span id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2" style="color:Red;visibility:hidden;">*</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="left" valign="top">
-                <strong>Teléfono:</strong></td>
-            <td align="left" valign="top">
-                <input name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$txtTelefono" type="text" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtTelefono" style="width:200px;" />
-            </td>
-            <td align="left" valign="top">
-                <span id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3" style="color:Red;visibility:hidden;">*</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="left" valign="top">
-                <strong>E-Mail: </strong>
-            </td>
-            <td align="left" valign="top">
-                <input name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$txtEmail" type="text" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtEmail" style="width:200px;" />
-            </td>
-            <td align="left" valign="top">
-                <span id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1" style="color:Red;visibility:hidden;">*</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="left" valign="top">
-                <strong>Mensaje: </strong>
-            </td>
-            <td align="left" valign="top">
-                <textarea name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$txtComentario" rows="2" cols="20" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtComentario" style="height:100px;width:200px;"></textarea>
-            </td>
-            <td align="left" valign="top">
-                <span id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4" style="color:Red;visibility:hidden;">*</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="right" valign="top">
-                <input type="submit" name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$Button2" value="Limpiar" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$Button2&quot;, &quot;&quot;, true, &quot;&quot;, &quot;&quot;, false, false))" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_Button2" />
-            </td>
-            <td align="left" valign="top">
-                <input type="submit" name="ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$Button1" value="Envia" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$ctl00$ctl00$ctl00$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContentPlaceHolderDefault$ContactForm_5$Button1&quot;, &quot;&quot;, true, &quot;&quot;, &quot;&quot;, false, false))" id="ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_Button1" />
-            </td>
-            <td align="left" valign="top">
-                &nbsp;</td>
-        </tr>
-    </table>
-
-<script type="text/javascript">
-//<![CDATA[
-var Page_Validators =  new Array(document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1"), document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2"), document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3"), document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1"), document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4"));
-//]]>
-</script>
-
-<script type="text/javascript">
-//<![CDATA[
-var ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1 = document.all ? document.all["ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1"] : document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1");
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1.controltovalidate = "ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtNombre";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1.errormessage = "*";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1.evaluationfunction = "RequiredFieldValidatorEvaluateIsValid";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator1.initialvalue = "";
-var ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2 = document.all ? document.all["ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2"] : document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2");
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2.controltovalidate = "ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtApellido";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2.errormessage = "*";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2.evaluationfunction = "RequiredFieldValidatorEvaluateIsValid";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator2.initialvalue = "";
-var ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3 = document.all ? document.all["ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3"] : document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3");
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3.controltovalidate = "ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtTelefono";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3.errormessage = "*";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3.evaluationfunction = "RequiredFieldValidatorEvaluateIsValid";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator3.initialvalue = "";
-var ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1 = document.all ? document.all["ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1"] : document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1");
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1.controltovalidate = "ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtEmail";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1.errormessage = "*";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1.evaluationfunction = "RegularExpressionValidatorEvaluateIsValid";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RegularExpressionValidator1.validationexpression = "\\w+([-+.\']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
-var ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4 = document.all ? document.all["ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4"] : document.getElementById("ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4");
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4.controltovalidate = "ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_txtComentario";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4.errormessage = "*";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4.evaluationfunction = "RequiredFieldValidatorEvaluateIsValid";
-ctl00_ctl00_ctl00_ctl00_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContentPlaceHolderDefault_ContactForm_5_RequiredFieldValidator4.initialvalue = "";
-//]]>
-</script>
-
-<div>
-
-	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="CA0B0334" />
+<div class="main">
+	<div id="banner">
+		<div class="text1">
+			AIR-LINES<span>Viajes Internacinoales</span><p>En donde viajar es sinonimo de seguridad y conformidad.</p>
+		</div>
+	</div>
+</div><div class="ic">More Website Templates at TemplateMonster.com!</div>
+<!-- / header -->
+<div class="main">
+<!-- content -->
+	<section id="content">
+		<article class="col1">
+			<div class="pad_1">
+				<h2>Contactos</h2>
+				<span class="cols">
+					País: Ecuador<br>
+					Ciudad: Machala<br>
+					Teléfono: +5939975103448<br>
+					Email: 
+				<a href="mailto:">caaguilar_est@utmachala.edu.ec</a>
+				<h2>Sobre nosotros </h2>
+				<p>Somos una agencia internacional de viajes aereos, tu elige el destino, nosotros nos encargamos de tu conformidad durante todo el vuelo. Estamos ubicados en la Ciudad de Machala, Provincia de El Oro, reserva tu vuelo y disfruta de la comodidad y la mejor atención durante todo tu recorrido</p>
+			</div>
+		</article>
+	</section>
+<!-- / content -->
 </div>
-
-<script type="text/javascript">
-//<![CDATA[
-
-var Page_ValidationActive = false;
-if (typeof(ValidatorOnLoad) == "function") {
-    ValidatorOnLoad();
-}
-
-function ValidatorOnSubmit() {
-    if (Page_ValidationActive) {
-        return ValidatorCommonOnSubmit();
-    }
-    else {
-        return true;
-    }
-}
-        //]]>
-</script>
-</form>
-</body>
-</html>>
-
-
 <?php include 'usuario/piepagina.php' ?>
--- Noticias.php
+++ Noticias.php
@@ -1,5 +1,34 @@
 <?php include 'usuario/cabecera.php' ?>
 
-
-
+<div class="main">
+	<div id="banner">
+		<div class="text1">
+			NOTIICIAS<span>AIR LINES</span><p>En donde viajar es sinonimo de seguridad y conformidad</p>
+		</div>
+	</div>
+</div><div class="ic">More Website Templates at TemplateMonster.com!</div>
+<!-- / header -->
+<div class="main">
+<!-- content -->
+	<section id="content">
+		<article class="col2 pad_left1">
+			<h2>Vuela a Sudamérica desde $1200</h2>
+			<div class="wrapper">
+				<figure class="left marg_right1"><img src="images/sudamerica.jpg" alt=""></figure>
+			</div>
+			<p>Vuela desde Madrid y Barcelona a  Sudamérica desde solo 669€. Visita  Chile, Ecuador, Paraguay y Perú   al mejor precio. Llegó el momento de conocer un continente mágico. 
+			¡Reserva ahora y vuela a Sudamérica con LAN y TAM Airlines!.</p>
+			<div class="wrapper"><a href="#" class="button1">Read More</a></div>
+			<h2>Cuba y EU reanudan vuelos comerciales luego de 50 años</h2>
+			<div class="wrapper">
+				<figure class="left marg_right1"><img src="images/cuba.jpg" alt=""></figure>
+				<p>Se calcula que para fines de este año decenas de vuelos estarán cubriendo la ruta
+				<p>EU y Cuba firmarán acuerdo para reanudar vuelos regulares</p>
+				<p>Cuba y EU sostendrán segunda ronda de negociaciones comerciales.</p>
+				
+			</div>
+		</article>
+	</section>
+<!-- / content -->
+</div>
 <?php include 'usuario/piepagina.php' ?>
